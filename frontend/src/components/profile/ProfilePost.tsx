f59f14b0ecb19d9fc5412401c075f68ffd404398
leesh235 <leeshgh235@gmail.com>
2022-03-22 20:59:02 +0900

    refactoring

---
+++ frontend/src/components/profile/ProfilePost.tsx
@@ -0,0 +1,45 @@
+import styled from "../../styles/theme-components";
+import { IntroduceCard } from "../card/IntroduceCard";
+import { ImageCard } from "../card/ImageCard";
+import { FriendCard } from "../card/FriendCard";
+import { WritePostCard } from "../card/WritePostCard";
+import { PostFlexCard } from "../card/PostFlexCard";
+import { WritePost } from "../modal/WritePost";
+
+const Wrapper = styled.section`
+    width: 908px;
+    padding: 0 16px;
+    margin-top: 16px;
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    column-gap: 16px;
+`;
+
+const LeftWrapper = styled.section`
+    width: 360px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const RightWrapper = styled.section`
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ProfilePost = () => {
+    return (
+        <Wrapper>
+            <LeftWrapper>
+                <IntroduceCard />
+                <ImageCard />
+                <FriendCard />
+            </LeftWrapper>
+            <RightWrapper>
+                <WritePostCard />
+                <PostFlexCard />
+            </RightWrapper>
+            {/* <WritePost /> */}
+        </Wrapper>
+    );
+};
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -5,143 +5,143 @@ import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { Text } from "../components/common/Text";
 import { IconButton } from "../components/common/button/IconButton";
-import { ProfilePost } from "../components/view/ProfilePost";
-import { ProfileInfo } from "../components/view/ProfileInfo";
-import { ProfileFriend } from "../components/view/ProfileFriend";
-import { ProfileImage } from "../components/view/ProfileImage";
-import { ProfileVideo } from "../components/view/ProfileVideo";
-import { ProfileCheckIn } from "../components/view/ProfileCheckIn";
+import { ProfilePost } from "../components/profile/ProfilePost";
+import { ProfileInfo } from "../components/profile/ProfileInfo";
+import { ProfileFriend } from "../components/profile/ProfileFriend";
+import { ProfileImage } from "../components/profile/ProfileImage";
+import { ProfileVideo } from "../components/profile/ProfileVideo";
+import { ProfileCheckIn } from "../components/profile/ProfileCheckIn";
 
 const Wrapper = styled.main`
-  background-color: ${(props) => props.theme.color.gray};
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 56px;
+    background-color: ${(props) => props.theme.color.gray};
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 56px;
 `;
 
 const Top = styled.section`
-  background-color: ${(props) => props.theme.color.white};
-  width: 100%;
-  height: 450px;
+    background-color: ${(props) => props.theme.color.white};
+    width: 100%;
+    height: 450px;
 `;
 
 const Center = styled.section`
-  background-color: ${(props) => props.theme.color.white};
-  width: 100%;
-  height: 60px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
+    background-color: ${(props) => props.theme.color.white};
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
 `;
 
 const MenuWrapper = styled.ul`
-  max-width: 908px;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  padding-top: 3px;
-  border-top: 1px solid ${(props) => props.theme.color.lightGray};
+    max-width: 908px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    padding-top: 3px;
+    border-top: 1px solid ${(props) => props.theme.color.lightGray};
 `;
 
 const Menu = styled.li<{ color: string }>`
-  border-bottom: 3px solid ${(props) => props.color};
+    border-bottom: 3px solid ${(props) => props.color};
 `;
 
 const menuList = [
-  "게시물",
-  "정보",
-  "친구",
-  "사진",
-  "동영상",
-  "체크인",
-  "더 보기",
+    "게시물",
+    "정보",
+    "친구",
+    "사진",
+    "동영상",
+    "체크인",
+    "더 보기",
 ];
 
 const menuUrl = [
-  "",
-  "#sk=about",
-  "#sk=friends",
-  "#sk=photos",
-  "#sk=videos",
-  "#sk=map",
-  "",
+    "",
+    "#sk=about",
+    "#sk=friends",
+    "#sk=photos",
+    "#sk=videos",
+    "#sk=map",
+    "",
 ];
 
 const Profile = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [click, setClick] = useState<number>(0);
-  const handleOnClick = ({ id }: { id: number }) => {
-    setClick(id);
-    navigate(`${menuUrl[id]}`, { replace: true });
-  };
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [click, setClick] = useState<number>(0);
+    const handleOnClick = ({ id }: { id: number }) => {
+        setClick(id);
+        navigate(`${menuUrl[id]}`, { replace: true });
+    };
 
-  useEffect(() => {}, [location]);
+    useEffect(() => {}, [location]);
 
-  return (
-    <Wrapper>
-      <Top>ProfileTop</Top>
-      <Center>
-        <MenuWrapper>
-          {menuList.map((val, idx) => {
-            if (click === idx) {
-              return (
-                <Menu
-                  id={val}
-                  onClick={() => {
-                    handleOnClick({ id: idx });
-                  }}
-                  key={idx}
-                  color={theme.color.seaBule}
-                >
-                  <IconButton width={"auto"} hover={false}>
-                    <Text
-                      text={val}
-                      fs={"15px"}
-                      fw={600}
-                      lh={"20px"}
-                      fc={theme.color.seaBule}
-                      width={"auto"}
-                    />
-                  </IconButton>
-                </Menu>
-              );
-            } else {
-              return (
-                <Menu
-                  id={val}
-                  onClick={() => {
-                    handleOnClick({ id: idx });
-                  }}
-                  key={idx}
-                  color={theme.color.white}
-                >
-                  <IconButton width={"auto"}>
-                    <Text
-                      text={val}
-                      fs={"15px"}
-                      fw={600}
-                      lh={"20px"}
-                      fc={theme.color.lightBlack}
-                      width={"auto"}
-                    />
-                  </IconButton>
-                </Menu>
-              );
-            }
-          })}
-        </MenuWrapper>
-      </Center>
-      {click === 0 && <ProfilePost />}
-      {click === 1 && <ProfileInfo />}
-      {click === 2 && <ProfileFriend />}
-      {click === 3 && <ProfileImage />}
-      {click === 4 && <ProfileVideo />}
-      {click === 5 && <ProfileCheckIn />}
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Top>ProfileTop</Top>
+            <Center>
+                <MenuWrapper>
+                    {menuList.map((val, idx) => {
+                        if (click === idx) {
+                            return (
+                                <Menu
+                                    id={val}
+                                    onClick={() => {
+                                        handleOnClick({ id: idx });
+                                    }}
+                                    key={idx}
+                                    color={theme.color.seaBule}
+                                >
+                                    <IconButton width={"auto"} hover={false}>
+                                        <Text
+                                            text={val}
+                                            fs={"15px"}
+                                            fw={600}
+                                            lh={"20px"}
+                                            fc={theme.color.seaBule}
+                                            width={"auto"}
+                                        />
+                                    </IconButton>
+                                </Menu>
+                            );
+                        } else {
+                            return (
+                                <Menu
+                                    id={val}
+                                    onClick={() => {
+                                        handleOnClick({ id: idx });
+                                    }}
+                                    key={idx}
+                                    color={theme.color.white}
+                                >
+                                    <IconButton width={"auto"}>
+                                        <Text
+                                            text={val}
+                                            fs={"15px"}
+                                            fw={600}
+                                            lh={"20px"}
+                                            fc={theme.color.lightBlack}
+                                            width={"auto"}
+                                        />
+                                    </IconButton>
+                                </Menu>
+                            );
+                        }
+                    })}
+                </MenuWrapper>
+            </Center>
+            {click === 0 && <ProfilePost />}
+            {click === 1 && <ProfileInfo />}
+            {click === 2 && <ProfileFriend />}
+            {click === 3 && <ProfileImage />}
+            {click === 4 && <ProfileVideo />}
+            {click === 5 && <ProfileCheckIn />}
+        </Wrapper>
+    );
 };
 
 export default Profile;
