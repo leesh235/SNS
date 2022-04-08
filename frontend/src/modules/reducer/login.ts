3e15d4999bef3b685f93db706a2eb7e36c5decb7
leesh235 <leeshgh235@gmail.com>
2022-04-08 21:16:42 +0900

    renamed

--- frontend/src/modules/reducer/login.ts
+++ frontend/src/modules/reducer/login.ts
@@ -1,4 +1,4 @@
-import { LOGIN, LOGIN_SUCCESS, LOGIN_ERROR } from "../action/auth";
+import { LOGIN, LOGIN_SUCCESS, LOGIN_ERROR } from "../action/login";
 import { handleAsyncReducer, reducerUtils } from "../../utils/reducerUtils";
 
 const initialState = {
@@ -9,22 +9,18 @@ const reducer = (state = initialState, action: any) => {
     const { type, data } = action;
     switch (type) {
         case LOGIN:
-            console.log("LOGIN");
             return {
                 ...state,
                 user: reducerUtils.loading(state.user),
             };
         case LOGIN_SUCCESS:
-            console.log("LOGIN_SUCCESS");
-            setTimeout(() => {
-                localStorage.setItem("token", data.accessToken);
-            }, 500);
+            localStorage.setItem("token", data.accessToken);
+            window.location.reload();
             return {
                 ...state,
                 user: reducerUtils.success(data),
             };
         case LOGIN_ERROR:
-            console.log("LOGIN_ERROR");
             return {
                 ...state,
                 user: reducerUtils.error(data),
