abf6914428dab16881f4687eddea96bd5bf9fdef
leesh235 <leeshgh235@gmail.com>
2022-04-22 20:44:46 +0900

    fix bug

--- backend/src/entity/Post.entity.ts
+++ backend/src/entity/Post.entity.ts
@@ -1,47 +1,41 @@
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
-  DeleteDateColumn,
-  OneToMany,
-  JoinColumn,
-  ManyToOne,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+    OneToMany,
+    JoinColumn,
+    ManyToOne,
 } from "typeorm";
 import { Comment } from "./comment.entity";
 import { User } from "./User.entity";
 
 @Entity("post")
 export class Post {
-  @PrimaryGeneratedColumn()
-  id!: number;
+    @PrimaryGeneratedColumn()
+    id!: number;
 
-  @Column({ type: "varchar", nullable: false })
-  title!: string;
+    @Column({ type: "varchar" })
+    contents: string;
 
-  @Column({ type: "varchar" })
-  contents: string;
+    @Column({ type: "varchar" })
+    files: string;
 
-  @Column({ type: "varchar" })
-  image: string;
+    @CreateDateColumn({ type: "timestamp", name: "create_date" })
+    createdAt: Date | undefined;
 
-  @Column({ type: "varchar" })
-  video: string;
+    @UpdateDateColumn({ type: "timestamp", name: "update_date" })
+    updatedAt: Date | undefined;
 
-  @CreateDateColumn({ type: "timestamp", name: "create_date" })
-  createdAt: Date | undefined;
+    @DeleteDateColumn({ type: "timestamp", name: "delete_date" })
+    deletedAt: Date | undefined;
 
-  @UpdateDateColumn({ type: "timestamp", name: "update_date" })
-  updatedAt: Date | undefined;
+    @OneToMany((type) => Comment, (comment) => comment.post)
+    comment: Comment;
 
-  @DeleteDateColumn({ type: "timestamp", name: "delete_date" })
-  deletedAt: Date | undefined;
-
-  @OneToMany((type) => Comment, (comment) => comment.post)
-  comment: Comment;
-
-  @ManyToOne((type) => User, (user) => user.post, { onDelete: "CASCADE" })
-  @JoinColumn({ name: "writer", referencedColumnName: "nickName" })
-  user: User;
+    @ManyToOne((type) => User, (user) => user.post, { onDelete: "CASCADE" })
+    @JoinColumn({ name: "writer", referencedColumnName: "email" })
+    user: User;
 }
