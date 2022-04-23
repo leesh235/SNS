a93a76565caefbe352203cf9522f84f6274d776b
leesh235 <leeshgh235@gmail.com>
2022-04-24 00:32:27 +0900

    add api

--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -18,4 +18,25 @@ export const api = {
         introduce: "/user/introduce",
         image: "/user/image",
     },
+    post: {
+        get: "/post/:postId",
+        write: "/post/write",
+        modify: "/post/modify",
+        delete: "/post/delete",
+        like: "/post/like",
+    },
+    posts: {
+        all_ist: "/posts/",
+        like_list: "/posts/like",
+        bookmark_list: "/posts/bookmark",
+        my_list: "/posts/:userId",
+        friends_list: "/posts/friends",
+    },
+    comment: {
+        get: "/comment/:postId",
+        write: "/comment/write",
+        modify: "/comment/modify",
+        delete: "/comment/delete",
+        like: "/comment/like",
+    },
 };
