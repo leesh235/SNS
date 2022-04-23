fd99a088bc01a5bfdef01393ab573d87f2a08986
leesh235 <leeshgh235@gmail.com>
2022-04-24 00:38:31 +0900

    connect api

--- frontend/src/components/view/PostList.tsx
+++ frontend/src/components/view/PostList.tsx
@@ -1,37 +1,54 @@
 import styled from "../../styles/theme-components";
 import { PostCard } from "../card/PostCard";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAllPosts } from "../../modules/action/posts";
 
 const Wrapper = styled.section`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, auto);
-  row-gap: 20px;
-  align-items: center;
-  padding: 0 20px;
-  margin-top: 16px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, auto);
+    row-gap: 20px;
+    align-items: center;
+    padding: 0 20px;
+    margin-top: 16px;
 `;
 
 const postList = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "2",
-  "3",
-  "4",
-  "2",
-  "3",
-  "4",
-  "2",
-  "3",
-  "4",
+    "1",
+    "2",
+    "3",
+    "4",
+    "2",
+    "3",
+    "4",
+    "2",
+    "3",
+    "4",
+    "2",
+    "3",
+    "4",
 ];
 
 export const PostList = () => {
-  return (
-    <Wrapper>
-      {postList.map((val, idx) => {
-        return <PostCard key={idx} />;
-      })}
-    </Wrapper>
-  );
+    const dispatch = useDispatch();
+    const { loading, data, error } = useSelector(
+        (state: any) => state.posts.allPosts
+    );
+
+    const [postList, setPostList] = useState<Array<any>>([]);
+
+    useEffect(() => {
+        if (data === null) {
+            dispatch(setAllPosts());
+        }
+        setPostList(data);
+    }, [loading]);
+
+    return (
+        <Wrapper>
+            {postList?.map((val, idx) => {
+                return <PostCard key={idx} post={val} />;
+            })}
+        </Wrapper>
+    );
 };
