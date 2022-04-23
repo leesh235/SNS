0a902d5d1c464661af9a5201b13c9e8700e1e9f1
leesh235 <leeshgh235@gmail.com>
2022-04-24 00:17:56 +0900

    add enum

--- backend/src/config/enums.ts
+++ backend/src/config/enums.ts
@@ -7,3 +7,10 @@ export enum LikeStatus {
     LIKE = "like",
     UNLIKE = "unlike",
 }
+
+export enum PostMode {
+    MY = "my",
+    LIKE = "like",
+    BOOKMARK = "bookmark",
+    FRIENDS = "friends",
+}
