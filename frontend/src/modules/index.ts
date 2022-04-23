1eb24d4dd6fb9c64025d82d3a379ed4bf8c82d1d
leesh235 <leeshgh235@gmail.com>
2022-04-24 00:32:53 +0900

    fix bug

--- frontend/src/modules/index.ts
+++ frontend/src/modules/index.ts
@@ -4,12 +4,22 @@ import { all } from "@redux-saga/core/effects";
 import login from "./reducer/login";
 import join from "./reducer/join";
 import profile from "./reducer/profile";
+import post from "./reducer/post";
+import posts from "./reducer/posts";
 //saga
 import { authSaga } from "./saga/auth";
 import { userSaga } from "./saga/user";
+import { postSaga } from "./saga/post";
+import { postsSaga } from "./saga/posts";
 
-export const rootReducer = combineReducers({ login, join, profile });
+export const rootReducer = combineReducers({
+    login,
+    join,
+    profile,
+    post,
+    posts,
+});
 
 export function* rootSaga() {
-    yield all([authSaga(), userSaga()]);
+    yield all([authSaga(), userSaga(), postSaga(), postsSaga()]);
 }
