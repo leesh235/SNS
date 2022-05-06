734420874634c6070f99caccf331c91f3ec13d9b
leesh235 <leeshgh235@gmail.com>
2022-05-07 02:55:33 +0900

    modify css

--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -3,24 +3,25 @@ import { SideMenu } from "../components/side/SideMenu";
 import { Welcome } from "../components/view/Welcome";
 
 const Wrapper = styled.main`
-  background-color: ${(props) => props.theme.color.gray};
-  width: 100%;
-  display: grid;
-  grid-template-columns: minmax(auto, 360px) auto minmax(auto, 360px);
-  padding-top: 56px;
-  > :nth-child(2) {
-    justify-items: center;
-    grid-column: 2 / span 1;
-  }
+    background-color: ${(props) => props.theme.color.gray};
+    width: 100%;
+    min-height: calc(100vh - 56px);
+    display: grid;
+    grid-template-columns: minmax(auto, 360px) auto minmax(auto, 360px);
+    padding-top: 56px;
+    > :nth-child(2) {
+        justify-items: center;
+        grid-column: 2 / span 1;
+    }
 `;
 
 const Start = () => {
-  return (
-    <Wrapper>
-      <SideMenu />
-      <Welcome />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <SideMenu />
+            <Welcome />
+        </Wrapper>
+    );
 };
 
 export default Start;
