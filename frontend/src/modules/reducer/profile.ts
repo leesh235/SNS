5a6b7cb637bdce5e4634e558576ce1cc8cadd8c7
leesh235 <leeshgh235@gmail.com>
2022-05-23 16:20:26 +0900

    refactoring

--- frontend/src/modules/reducer/profile.ts
+++ frontend/src/modules/reducer/profile.ts
@@ -13,7 +13,7 @@ import {
     COVERIMAGE_ERROR,
 } from "../action/profile";
 
-import { reducerUtils } from "../../utils/reducerUtils";
+import { handleAsyncReducer, reducerUtils } from "../../utils/reducerUtils";
 
 const initialState = {
     profile: reducerUtils.initial(null),
@@ -22,24 +22,19 @@ const initialState = {
 const reducer = (state = initialState, action: any) => {
     const { type, data } = action;
     switch (type) {
-        case PROFILE || INTRODUCE || PROFILEIMAGE || COVERIMAGE:
-            return {
-                ...state,
-                profile: reducerUtils.loading(state.profile.data),
-            };
+        case PROFILE:
         case PROFILE_SUCCESS:
-            return {
-                ...state,
-                profile: reducerUtils.success(data),
-            };
-        case PROFILE_ERROR ||
-            INTRODUCE_ERROR ||
-            PROFILEIMAGE_ERROR ||
-            COVERIMAGE_ERROR:
-            return {
-                ...state,
-                profile: reducerUtils.error(data),
-            };
+        case PROFILE_ERROR:
+            return handleAsyncReducer(PROFILE, "profile", true)(state, action);
+        case INTRODUCE:
+        case INTRODUCE_ERROR:
+            return handleAsyncReducer(PROFILE, "profile", true)(state, action);
+        case PROFILEIMAGE:
+        case PROFILEIMAGE_ERROR:
+            return handleAsyncReducer(PROFILE, "profile", true)(state, action);
+        case COVERIMAGE:
+        case COVERIMAGE_ERROR:
+            return handleAsyncReducer(PROFILE, "profile", true)(state, action);
         case INTRODUCE_SUCCESS:
             return {
                 ...state,
