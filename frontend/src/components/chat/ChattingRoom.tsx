30deedf546c331a1901992895cfe61bcd96f8acd
leesh235 <leeshgh235@gmail.com>
2022-06-29 17:02:14 +0900

    채팅방 나가기 기능 추가

--- frontend/src/components/chat/ChattingRoom.tsx
+++ frontend/src/components/chat/ChattingRoom.tsx
@@ -1,13 +1,16 @@
 import styled from "../../styles/theme-components";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
     setMessageList,
     setJoinRoom,
     setJoinRoomList,
+    setDeleteRoom,
+    setRoomList,
 } from "../../modules/action/chat";
 import { response, request, event } from "../../utils/socket";
 import { Text } from "../common/Text";
+import { Button2 } from "../common/button/Button2";
 
 const Wrapper = styled.div`
     width: 338px;
@@ -19,7 +22,25 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: space-between;
     box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+    position: relative;
 `;
+
+const SideWrapper = styled.div`
+    width: calc(200px - 30px);
+    height: auto;
+    padding: 15px;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 4px 8px rgb(0 0 0 / 20%);
+    background-color: ${(props) => props.theme.color.white};
+    position: absolute;
+    right: 350px;
+    display: flex;
+    flex-direction: column;
+    > :nth-child(n) {
+        margin: 3px 0;
+    }
+`;
+
 const Title = styled.div`
     width: calc(100% - 16px);
     height: 32px;
@@ -54,17 +75,15 @@ const MessageList = styled.div`
 const MyMessageWrapper = styled.div`
     width: 100%;
     display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
+    flex-direction: column-reverse;
+    align-items: flex-end;
 `;
 
 const MessageWrapper = styled.div`
     width: 100%;
     display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
+    flex-direction: column;
+    align-items: flex-start;
 `;
 
 const Message = styled.div`
@@ -76,6 +95,8 @@ const Message = styled.div`
     border-radius: 18px;
     background-color: ${(props) => props.theme.color.gray};
     padding: 8px 12px;
+    margin-top: 5px;
+    margin-bottom: 10px;
     a {
         width: auto;
     }
@@ -121,6 +142,8 @@ export const ChattingRoom = ({ roomId, roomName }: Props) => {
         (state: any) => state.chat.messageList
     );
 
+    const [open, setOpen] = useState<boolean>(false);
+
     const createDom = (parent: any, tag: any, data: any) => {
         let list: any = document.getElementById(parent);
         let item = document.createElement(tag);
@@ -158,6 +181,12 @@ export const ChattingRoom = ({ roomId, roomName }: Props) => {
         dispatch(setJoinRoomList({ roomId: roomId, title: roomName }));
     };
 
+    const handleDeleteRoom = () => {
+        dispatch(setDeleteRoom({ roomId }));
+        dispatch(setRoomList());
+        handleLeave();
+    };
+
     useEffect(() => {
         handleScroll();
         dispatch(setMessageList({ roomId }));
@@ -170,7 +199,12 @@ export const ChattingRoom = ({ roomId, roomName }: Props) => {
     return (
         <Wrapper>
             <Title>
-                <div>{roomName}</div>
+                <Button2
+                    text={`${roomName}`}
+                    onClick={() => {
+                        setOpen(!open);
+                    }}
+                />
                 <div>
                     <LeaveBtn
                         onClick={() => {
@@ -228,6 +262,17 @@ export const ChattingRoom = ({ roomId, roomName }: Props) => {
             <ChatForm onSubmit={handleChatting}>
                 <ChatInput name="chatInput" placeholder="Aa" autoFocus />
             </ChatForm>
+            {open && (
+                <SideWrapper>
+                    {/* <Button2 text={"친구 초대"} width={"100%"} />
+                    <Button2 text={"이름 변경"} width={"100%"} /> */}
+                    <Button2
+                        text={"채팅 삭제"}
+                        width={"100%"}
+                        onClick={handleDeleteRoom}
+                    />
+                </SideWrapper>
+            )}
         </Wrapper>
     );
 };
