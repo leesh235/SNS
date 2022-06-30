504fdaea0af2908576916d9e556ac48c2450217f
leesh235 <leeshgh235@gmail.com>
2022-06-30 23:54:15 +0900

    이미지 캐로셀 기능 추가

--- frontend/src/components/detail/ImageView.tsx
+++ frontend/src/components/detail/ImageView.tsx
@@ -1,8 +1,9 @@
 import styled from "../../styles/theme-components";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LogoIcon } from "../../assets/icon/LogoIcon";
+import { Text } from "../common/Text";
 
 const Wrapper = styled.section`
     width: 100%;
@@ -23,12 +24,21 @@ const Top = styled.div`
 `;
 
 const Contents = styled.article`
-    width: 100%;
+    width: calc(100% - 40px);
     height: calc(100% - 112px);
-    padding: 0 0 56px 0;
+    padding: 0 20px 56px 20px;
     display: flex;
     align-items: center;
     justify-content: center;
+    > div {
+        display: none;
+    }
+    :hover {
+        justify-content: space-between;
+        > div {
+            display: flex;
+        }
+    }
 `;
 
 const Image = styled.img``;
@@ -43,18 +53,46 @@ const CloseBtn = styled.div`
     cursor: pointer;
 `;
 
+const NextPreBtn = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 48px;
+    height: 48px;
+    border-radius: 24px;
+    background-color: rgb(228, 230, 235, 0.5);
+    cursor: pointer;
+    :hover {
+        background-color: ${(props) => props.theme.color.white};
+    }
+`;
+
 export const ImageView = () => {
+    const navigate = useNavigate();
+
     const { loading, data, error } = useSelector(
         (state: any) => state?.post?.postDetail
     );
 
-    const navigate = useNavigate();
+    const [select, setSelect] = useState<number>(0);
 
     const handleClose = () => {
         navigate(-1);
     };
 
-    useEffect(() => {}, [loading]);
+    const handleNext = () => {
+        if (data?.images.length - 1 <= select) setSelect(0);
+        else setSelect((prev) => prev + 1);
+    };
+
+    const handlePrev = () => {
+        if (0 >= select) setSelect(data?.images.length - 1);
+        else setSelect((prev) => prev - 1);
+    };
+
+    useEffect(() => {
+        console.log(data?.images);
+    }, [loading]);
 
     return (
         <Wrapper>
@@ -63,7 +101,13 @@ export const ImageView = () => {
                 <LogoIcon />
             </Top>
             <Contents>
-                <Image src={data?.images[0].url} />
+                <NextPreBtn onClick={handlePrev}>
+                    <Text text={"<"} fs={"20px"} fw={600} width={"auto"} />
+                </NextPreBtn>
+                <Image src={data?.images[select].url} />
+                <NextPreBtn onClick={handleNext}>
+                    <Text text={">"} fs={"20px"} fw={600} width={"auto"} />
+                </NextPreBtn>
             </Contents>
         </Wrapper>
     );
