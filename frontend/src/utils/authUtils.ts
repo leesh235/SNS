62fc7610b59f7766bd41d374fa749047c42e2913
leesh235 <leeshgh235@gmail.com>
2022-07-21 21:26:27 +0900

    버그수정(특정페이지에서 로그아웃시 흰화면 오류)

--- frontend/src/utils/authUtils.ts
+++ frontend/src/utils/authUtils.ts
@@ -1,6 +1,6 @@
 export const logOut = () => {
     window.localStorage.removeItem("token");
     setTimeout(() => {
-        window.location.reload();
+        window.location.replace("/");
     }, 50);
 };
