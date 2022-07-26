22462951095e430082c8e9dd87c4f39fbdb5621b
leesh235 <leeshgh235@gmail.com>
2022-07-26 22:57:29 +0900

    버그 수정(배경 높이 수정)

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -11,6 +11,7 @@ import { ChattingList } from "../components/main/ChattingList";
 const Wrapper = styled.main`
     background-color: ${(props) => props.theme.color.gray};
     width: 100%;
+    min-height: calc(100vh - 56px);
     display: grid;
     grid-template-columns: minmax(auto, 360px) auto minmax(auto, 360px);
     padding-top: 56px;
