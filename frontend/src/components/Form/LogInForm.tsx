03edda01c1948fe54842203972f3f70062aaeb07
leesh235 <leeshgh235@gmail.com>
2022-12-22 22:43:16 +0900

    refactoring(add custom hooks)

--- frontend/src/components/Form/LogInForm.tsx
+++ frontend/src/components/Form/LogInForm.tsx
@@ -1,15 +1,18 @@
 import styled from "../../styles/theme-components";
-import { useEffect, useState } from "react";
+import theme from "../../styles/theme";
+import { useDispatch } from "react-redux";
+//functions
+import { routes } from "../../utils/routes";
+import { setLogin } from "../../modules/action/auth";
+import { authMessage } from "../../utils/message";
+import { useForm } from "../../hooks/useForm";
+import { useModal } from "../../hooks/useModal";
+//components
 import { Input } from "../common/input/Input";
 import { Button } from "../common/button/Button";
 import { Join } from "../modal/Join";
 import { Line } from "../common/Line";
 import { LinkText } from "../common/button/LinkText";
-import { routes } from "../../utils/routes";
-import theme from "../../styles/theme";
-import { useDispatch, useSelector } from "react-redux";
-import { setLogin } from "../../modules/action/auth";
-import { authMessage } from "../../utils/message";
 
 const Wrapper = styled.section`
     width: 396px;
@@ -44,73 +47,72 @@ const FormWrapper = styled.form`
 `;
 
 export const LogInForm = () => {
-    const [open, setOpen] = useState<boolean>(false);
     const dispatch = useDispatch();
-    const { loading, data, error } = useSelector(
-        (state: any) => state?.auth?.user
-    );
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
+    const { modal, onModalClick } = useModal();
 
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const handleLogin: React.FormEventHandler<HTMLFormElement> = async (e) => {
-        e.preventDefault();
-        const { email, password } = e.currentTarget;
-        dispatch(setLogin({ email: email.value, password: password.value }));
-    };
-
-    useEffect(() => {
-        if (error !== null) {
-            alert(authMessage.login.login_error);
-        }
-        if (!error && data !== null) {
-            localStorage.setItem("token", data?.accessToken);
-            window.location.reload();
-        }
-    }, [loading]);
+    const { errors, setOption, handleSubmit } = useForm({
+        initValues: "",
+        validate: "",
+        stateFunc: (state: any) => state.auth?.user,
+        onSubmit: (formData: any) => {
+            dispatch(
+                setLogin({
+                    email: formData?.email,
+                    password: formData?.password,
+                })
+            );
+        },
+        result: (data: any, error: any) => {
+            if (error) {
+                alert(authMessage.login.login_error);
+            }
+            if (data) {
+                localStorage.setItem("token", data?.accessToken);
+                window.location.reload();
+            }
+        },
+    });
 
     return (
-        <Wrapper>
-            <BoxWrapper>
-                <FormWrapper onSubmit={handleLogin}>
-                    <Input
-                        name={"email"}
-                        padding={"14px 16px"}
-                        placeholder={"이메일 또는 전화번호"}
-                        required={true}
+        <>
+            <Wrapper>
+                <BoxWrapper>
+                    <FormWrapper onSubmit={handleSubmit}>
+                        <Input
+                            {...setOption("email")}
+                            cssObj={{ padding: "14px 16px" }}
+                            placeholder={"이메일 또는 전화번호"}
+                            required={true}
+                        />
+                        <Input
+                            {...setOption("password")}
+                            cssObj={{ padding: "14px 16px" }}
+                            placeholder={"비밀번호"}
+                            type={"password"}
+                            required={true}
+                        />
+                        <Button text="로그인" />
+                        <LinkText
+                            to={routes.forget}
+                            text={"비밀번호를 잊으셨나요?"}
+                            color={theme.color.seaBule}
+                        />
+                        <Line />
+                    </FormWrapper>
+                    <Button
+                        text={"새 계정 만들기"}
+                        width={"144px"}
+                        height={"48px"}
+                        color={theme.color.lightGreen}
+                        fs={"17px"}
+                        type={"button"}
+                        onClick={onModalClick}
                     />
-                    <Input
-                        name={"password"}
-                        padding={"14px 16px"}
-                        placeholder={"비밀번호"}
-                        type={"password"}
-                        required={true}
-                    />
-                    <Button text="로그인" />
-                    <LinkText
-                        to={routes.forget}
-                        text={"비밀번호를 잊으셨나요?"}
-                        color={theme.color.seaBule}
-                    />
-                    <Line />
-                </FormWrapper>
-                <Button
-                    text={"새 계정 만들기"}
-                    width={"144px"}
-                    height={"48px"}
-                    color={theme.color.lightGreen}
-                    fs={"17px"}
-                    type={"button"}
-                    onClick={handleOpen}
-                />
-                {open && <Join onClose={handleClose} />}
-            </BoxWrapper>
-            <LinkText to={routes.forget} text={"페이지 만들기."} fw={600} />
-        </Wrapper>
+                </BoxWrapper>
+                <LinkText to={routes.forget} text={"페이지 만들기."} fw={600} />
+            </Wrapper>
+            {modal && <Join onClose={onModalClick} />}
+        </>
     );
 };
