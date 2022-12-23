7cf71e0e9c33adb046a8259201f9ee3969151f90
leesh235 <leeshgh235@gmail.com>
2022-12-24 00:10:31 +0900

    fix bug

--- frontend/src/utils/Calendar.ts
+++ frontend/src/utils/Calendar.ts
@@ -1,16 +1,18 @@
-export const getNowDate = () => {
+const monthArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const lastDayArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+export const calendarUtil = () => {
     const now = new Date();
 
     const year = now.getFullYear();
-    const startYear = year - 120;
-    const years = new Array(year - startYear).fill(0);
-
     const month = now.getMonth() + 1;
-    const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
     const day = now.getDate();
-    const mon_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const days = new Array(mon_days[month]).fill(0);
+
+    const years = new Array(120).fill(0).map((_, idx) => year - idx);
+    const months = monthArray;
+    const days = new Array(lastDayArray[month - 1])
+        .fill(0)
+        .map((_, idx) => idx);
 
     return { year, month, day, years, months, days };
 };
