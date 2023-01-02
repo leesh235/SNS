878d55b98107413c752e804bc1c2b5e85b297713
leesh235 <leeshgh235@gmail.com>
2023-01-02 21:05:16 +0900

    add validate

--- frontend/src/types/validate.ts
+++ frontend/src/types/validate.ts
@@ -2,3 +2,15 @@ export interface LoginErrorType {
     email?: string;
     password?: string;
 }
+
+export interface FindPwErrorType {
+    email?: string;
+}
+
+export interface VerifyCodeNumberType {
+    codeNumber?: string;
+}
+
+export interface ModifyPwErrorType {
+    password?: string;
+}
--- frontend/src/utils/validate.ts
+++ frontend/src/utils/validate.ts
@@ -1,9 +1,40 @@
-import { LoginErrorType } from "../types/validate";
+import {
+    LoginErrorType,
+    FindPwErrorType,
+    VerifyCodeNumberType,
+    ModifyPwErrorType,
+} from "../types/validate";
 
 export const loginValidate = ({ email, password }: LoginErrorType) => {
     const errors: LoginErrorType = {};
 
     if (!email) errors.email = "required";
+    if (!password) errors.password = "required";
+
+    return errors;
+};
+
+export const findPasswordValidate = ({ email }: FindPwErrorType) => {
+    const errors: FindPwErrorType = {};
+
+    if (!email) errors.email = "required";
+
+    return errors;
+};
+
+export const verifyCodeNumberValidate = ({
+    codeNumber,
+}: VerifyCodeNumberType) => {
+    const errors: VerifyCodeNumberType = {};
+
+    if (!codeNumber) errors.codeNumber = "required";
+
+    return errors;
+};
+
+export const modifyPasswordValidate = ({ password }: ModifyPwErrorType) => {
+    const errors: ModifyPwErrorType = {};
+
     if (!password) errors.password = "required";
 
     return errors;
