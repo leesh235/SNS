e4afc7c09b20360d647a1dd76e56c262099db5e6
leesh235 <leeshgh235@gmail.com>
2023-01-03 21:48:59 +0900

    add redux devtools

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,10 +9,14 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import { rootReducer, rootSaga } from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
