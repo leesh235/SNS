1be6f6189cc5c75b009b86cc28ac6af6d78d834a
leesh235 <leeshgh235@gmail.com>
2023-01-04 21:30:10 +0900

    add jwt saga util

--- frontend/src/utils/asyncUtils.ts
+++ frontend/src/utils/asyncUtils.ts
@@ -1,12 +1,31 @@
 import { call, put } from "redux-saga/effects";
-import { ServerResponse } from "http";
+import { AxiosResponse } from "axios";
 
 export const createPromise = (type: string, pomiseCreator: any) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    return function* saga(action: any) {
+        try {
+            const { data }: AxiosResponse = yield call(
+                pomiseCreator,
+                action.data
+            );
+            yield put({ type: SUCCESS, data });
+        } catch (error) {
+            yield put({ type: ERROR, error: true, data: error });
+        }
+    };
+};
+
+export const createJWTPromise = (type: string, pomiseCreator: any) => {
     const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
     return function* saga(action: any) {
         try {
-            const data: ServerResponse = yield call(pomiseCreator, action.data);
+            const { data }: AxiosResponse = yield call(
+                pomiseCreator,
+                action.data
+            );
+            localStorage.setItem("token", data.accessToken);
             yield put({ type: SUCCESS, data });
         } catch (error) {
             yield put({ type: ERROR, error: true, data: error });
