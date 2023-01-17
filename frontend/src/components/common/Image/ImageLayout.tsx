a254e75138939d359d17446e10c510be1a8be5c0
leesh235 <leeshgh235@gmail.com>
2023-01-17 16:41:03 +0900

    add post card layouts

---
+++ frontend/src/components/common/Image/ImageLayout.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import styled from "../../../styles/theme-components";
+import theme from "../../../styles/theme";
+import { Link } from "react-router-dom";
+import { routes } from "../../../utils/routes";
+import { Text } from "../Text";
+
+const Layout = styled.div<{ cnt?: any }>`
+    width: 100%;
+    height: ${(props) => (props.cnt > 1 ? "590px" : "100%")};
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    align-content: space-between;
+    position: relative;
+`;
+
+const Image = styled.img<{ cnt?: any }>`
+    width: ${(props) => `calc(${99 / props.cnt}%)`};
+    height: ${(props) => `calc(${99 / props.cnt}%)`};
+    max-height: 500px;
+`;
+
+const Shadow = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: calc(99% / 2);
+    height: calc(99% / 2);
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(96, 103, 112, 0.5);
+`;
+
+interface Props {
+    post: {
+        postId: number;
+        images: Array<string>;
+    };
+}
+
+export const ImageLayout = ({ post }: Props) => {
+    return (
+        <Link
+            to={{
+                pathname: `${routes.detail}${post?.postId}`,
+            }}
+            style={{
+                width: "100%",
+                height: "100%",
+                maxHeight: "590px",
+            }}
+        >
+            <Layout cnt={post?.images.length}>
+                {post?.images.map((val: any, idx: number) => {
+                    if (idx < 4)
+                        return (
+                            <Image
+                                key={val.id}
+                                cnt={
+                                    post?.images && post?.images?.length > 1
+                                        ? 2
+                                        : 1
+                                }
+                                src={`${val.url}`}
+                            />
+                        );
+                })}
+                {post?.images.length > 4 && (
+                    <Shadow>
+                        <Text
+                            text={`+${post?.images.length - 4}장`}
+                            tag={"span"}
+                            cssObj={{
+                                fontWeight: 550,
+                                fontColor: theme.color.white,
+                                fontSize: "45px",
+                            }}
+                        />
+                    </Shadow>
+                )}
+            </Layout>
+        </Link>
+    );
+};
---
+++ frontend/src/components/common/SeeMoreLayout.tsx
@@ -0,0 +1,50 @@
+import styled from "../../styles/theme-components";
+import { useEffect } from "react";
+import { useModal } from "../../hooks/common/useModal";
+import { HoverBtn } from "./button/HoverBtn";
+import { CloseEventBtn } from "./button/CloseEventBtn";
+import { MoreIcon } from "../../assets/icon/MoreIcon";
+
+const Layout = styled.div`
+    width: 36px;
+    height: 36px;
+    border-radius: 18px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    :hover {
+        background-color: ${(props) => props.theme.color.gray1};
+    }
+    cursor: pointer;
+    position: relative;
+`;
+
+const SeeMore = styled.span<{ width?: string }>`
+    width: ${(props) => props.width || "344px"};
+    height: auto;
+    padding: 8px;
+    border-radius: 6px;
+    border: 0px;
+    background-color: ${(props) => props.theme.color.white};
+    box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+    position: absolute;
+    top: 45px;
+    right: 0px;
+    z-index: 9;
+`;
+
+interface Props {
+    children: React.ReactNode;
+    width?: string;
+}
+
+export const SeeMoreLayout = ({ children, width }: Props) => {
+    const { modal, handleModal } = useModal();
+
+    return (
+        <Layout onClick={handleModal}>
+            <MoreIcon />
+            {modal && <SeeMore width={width}>{children}</SeeMore>}
+        </Layout>
+    );
+};
