86e43ba2e888a1332a46cc75924933340f357c40
leesh235 <leeshgh235@gmail.com>
2023-01-18 19:40:02 +0900

    add comment list hook

---
+++ frontend/src/hooks/common/useGetComment.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { commentActionCreator } from "../../modules/action/comment";
+
+export const useGetComment = (id: number) => {
+    const dispatch = useDispatch();
+    const { loading, data, error } = useSelector((state: any) => {
+        if (!state.comment[id])
+            return { loading: false, data: null, error: "" };
+        return state.comment[id];
+    });
+
+    useEffect(() => {
+        dispatch(commentActionCreator.list({ postId: id }));
+    }, [dispatch]);
+
+    return { loading, data, error };
+};
