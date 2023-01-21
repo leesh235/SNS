a64d0263d53a562c82c16555b81afbb8bff2c311
leesh235 <leeshgh235@gmail.com>
2023-01-21 23:07:30 +0900

    hover 기능 추가

--- frontend/src/assets/icon/MoreIcon.tsx
+++ frontend/src/assets/icon/MoreIcon.tsx
@@ -1,13 +1,30 @@
+import styled from "../../styles/theme-components";
+
+const Layout = styled.span`
+    width: 36px;
+    height: 36px;
+    border-radius: 18px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    :hover {
+        background-color: ${(props) => props.theme.color.lightGray};
+    }
+`;
+
 export const MoreIcon = () => {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-        >
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M5 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm14 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-7 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
-        </svg>
+        <Layout>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+            >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path d="M5 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm14 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-7 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
+            </svg>
+        </Layout>
     );
 };
