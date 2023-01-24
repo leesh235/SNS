7eb0368832b666483135a4e0fee6673fd0c3b2d3
leesh235 <leeshgh235@gmail.com>
2023-01-24 16:22:56 +0900

    add types

--- frontend/src/types/lib/profile.ts
+++ frontend/src/types/lib/profile.ts
@@ -11,3 +11,29 @@ export interface UserImage {
 export interface Take {
     take?: number;
 }
+
+export interface AbilityType {
+    id?: number;
+    name: string;
+    position?: string;
+    address?: string;
+    start?: string;
+    end?: string;
+}
+
+export interface SchoolType {
+    id?: number;
+    name: string;
+    start?: string;
+    end?: string;
+    status?: boolean;
+}
+
+export interface UniversityType {
+    id?: number;
+    name: string;
+    major?: string;
+    degree?: string;
+    start?: string;
+    end?: string;
+}
