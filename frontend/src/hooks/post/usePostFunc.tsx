ddbe0e348295495d567ae30fcc42aaec12d7d918
leesh235 <leeshgh235@gmail.com>
2023-01-25 15:47:15 +0900

    add write function

--- frontend/src/hooks/post/usePostFunc.tsx
+++ frontend/src/hooks/post/usePostFunc.tsx
@@ -1,22 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postActionCreator } from "../../modules/action/post";
 
 export const usePostFunc = (postId: number) => {
     const dispatch = useDispatch();
 
-    const handleLike: React.MouseEventHandler = (e) => {
+    const handleLike: React.MouseEventHandler = useCallback(() => {
         dispatch(postActionCreator.like({ postId }));
-    };
+    }, [dispatch]);
 
-    const handleDelete: React.MouseEventHandler = (e) => {
+    const handleDelete = useCallback(() => {
         if (
             window.confirm(
                 "휴지통으로 보내시겠습니까?(30일 후에 영구 삭제됩니다.)"
             )
         )
             dispatch(postActionCreator.delete({ postId }));
-    };
+    }, [dispatch]);
 
-    return { handleLike, handleDelete };
+    const handleWrite = useCallback(
+        (formData: any) => {
+            dispatch(postActionCreator.delete({ postId }));
+        },
+        [dispatch]
+    );
+
+    return { handleLike, handleDelete, handleWrite };
 };
